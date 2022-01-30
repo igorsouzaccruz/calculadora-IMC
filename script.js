f387707765e6a2181f8a89f54251882cc4238f0a
igorsouzaccruz <igorsouzaccruz@gmail.com>
2022-01-30 20:29:31 -0300

    Resultados tabela e idade

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ function sexo(sexo){
         document.getElementById('mulher').style.background = 'white';
         document.getElementById('mulher').style.color = 'black';
         return sex = 'homem';
-        
     } 
 }
 
@@ -27,9 +26,11 @@ function calcular(){
     let p = Number(peso.value)
     let a = Number(altura.value)
     let i = Number(idade.value)
-    var IMC = p /(a * a)
+    if( 20 < i < 59){
+        alert("ERRO idadeException! A faixa de idade deve ser entre: 20 e 59 anos. ")
+    }
 
-    //document.getElementById('res').innerHTML = `O sexo é ${sex} `
+    var IMC = p /(a * a) //peso / (altura * altura)
 
     if(sex == 'homem'){
     document.getElementById('res').innerHTML = 
@@ -42,23 +43,23 @@ function calcular(){
             <td>IMC</td>
             <td>Categoria</td>
         </tr>
-        <tr>
+        <tr id = "abaixo">
             <td>Abaixo de 20,7 </td>
             <td>Abaixo do Peso</td>
         </tr>
-        <tr>
+        <tr id = "ideal">
             <td>20,7 a 26,4 </td>
             <td>Peso Ideal</td>
         </tr>
-        <tr>
+        <tr id = "acima">
             <td>26,5 a 27,8 </td>
             <td>Pouco acima do peso</td>
         </tr>
-        <tr>
+        <tr id = "grande">
             <td>27,9 a 31,1 </td>
             <td>Acima do peso</td>
         </tr>
-        <tr>
+        <tr id = "obeso">
             <td>31,2 e acima </td>
             <td>Obesidade</td>
         </tr>
@@ -66,34 +67,55 @@ function calcular(){
     }
     else{
     document.getElementById('res').innerHTML = 
-    `<p>Feminino - Se resultado de IMC foi <strong>${IMC.toFixed(2)} kg/m2</strong></p>
+    `<p>Feminino - Seu resultado: </p>
+    <p> IMC de <strong>${IMC.toFixed(2)} kg/m2.</strong></p>
     
     <table>
         <tr>
             <td>IMC</td>
             <td>Categoria</td>
         </tr>
-        <tr>
+        <tr id = "abaixo">
             <td>Abaixo de 19,1 </td>
             <td>Abaixo do Peso</td>
         </tr>
-        <tr>
+        <tr id = "ideal">
             <td>19,1 a 25,8 </td>
             <td>Peso Ideal</td>
         </tr>
-        <tr>
+        <tr id = "acima">
             <td>25,9 a 27,3 </td>
             <td>Pouco acima do peso</td>
         </tr>
-        <tr>
+        <tr d = "grande">
             <td>27,4 a 32,3 </td>
             <td>Acima do peso</td>
         </tr>
-        <tr>
+        <tr id = "obeso">
             <td>32,4 e acima </td>
             <td>Obesidade</td>
         </tr>
     </table>`
     }   
+
+    /*switch(IMC){
+        case (IMC <= 20,7):
+            document.getElementById('abaixo').style.background = "orange";
+            document.getElementById('abaixo').style.color = 'white';
+        case (20,7 < IMC <= 26,4):
+            document.getElementById('ideal').style.background = "orange";
+            document.getElementById('ideal').style.color = 'white';
+        case (26,4 < IMC <= 27,8):
+            document.getElementById('acima').style.background = "orange";
+            document.getElementById('acima').style.color = 'white';
+        case (27,8 < IMC <= 31,1):
+            document.getElementById('grande').style.background = "orange";
+            document.getElementById('grande').style.color = 'white';
+        case (31,1 < IMC):
+            document.getElementById('obeso').style.background = "orange";
+            document.getElementById('obeso').style.color = 'white';
+        default:
+            document.getElementById('res').innerHTML += `Fora de escala ou valor incorreto!!`
+    }*/
 }
 
